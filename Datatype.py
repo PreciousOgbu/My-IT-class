d4b6cd58dca9bbf199d4981d76975b16941774ca
PreciousOgbu <preciousogbu.me@gmail.com>
2024-07-19 18:25:16 +0100

    finally done the datatpye task

--- Datatype.py
+++ Datatype.py
@@ -1,4 +1,16 @@
 # To check data tyypes
-def check_data_types(my_lst):
-    data_types = type(item) for  item in my_lst:
+def check_data_types(lst):
+    if not isinstance(lst, list):
+        return 'This is not a list'
+    
+    first_item = type(lst[0])
+    if len(lst) == 1:
+        return 'You only have one item in the list, Nothing to compare'
+    
+    for item in lst:
+        if type(item) != first_item:
+            return False
         
+    return True
+
+print(check_data_types(['5', 'Presh']))
